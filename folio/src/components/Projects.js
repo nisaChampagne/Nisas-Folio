4ed1f465692dce10cfe5779515ea26323abdfadc
nisaChampagne <50988313+nisaChampagne@users.noreply.github.com>
2019-08-21 17:44:31 -0400

    working

--- folio/src/components/Projects.js
+++ folio/src/components/Projects.js
@@ -4,13 +4,24 @@ export default function Projects(){
     return(
         <div>
             <div className='projects'>
-                <a href='https://github.com/nisaChampagne/React-Sorting-Hat'> Hogwarts Sorting Hat Quiz</a>
+                <h3>Hogwarts Sorting Hat Quiz</h3>
+                <a href='https://github.com/nisaChampagne/React-Sorting-Hat'>GitHub Link</a>
+                <a href='https://react-sorting-hat.nchampag23.now.sh/'>Hosted</a>
             </div>
             <div className='projects'>
-                <a href='https://github.com/nisaChampagne/React-Todo'>React Todo List</a>
+            <h3>React Todo List</h3>
+                <a href='https://github.com/nisaChampagne/React-Todo'>Github Link </a>
+                <a href='https://react-todo-4bm2326a5.now.sh/'>Hosted</a>
             </div>
             <div className='projects'>
-                <a href='https://github.com/nisaChampagne/Sideprojectpokedex'>Simple PokeDex</a>
+                <h3>Simple PokeDex</h3>
+                <a href='https://github.com/nisaChampagne/Sideprojectpokedex'>Github Link</a>
+                <a href='https://pokedex.nchampag23.now.sh/'>Hosted</a>
+            </div>
+            <div className='projects'>
+                <h3>Client Side Authentication Practice</h3>
+                <a href='https://github.com/nisaChampagne/Auth-Friends'>Github Link</a>
+                <a href='https://github.com/nisaChampagne/Auth-Friends'>Hosted(placeholder)</a>
             </div>
         </div>
     )

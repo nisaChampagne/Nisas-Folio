2ca494452bd4cd8a396d5fb820272032a721fc8b
nisaChampagne <50988313+nisaChampagne@users.noreply.github.com>
2019-08-21 18:39:44 -0400

    projects imgs added

--- folio/src/components/Home.js
+++ folio/src/components/Home.js
@@ -15,6 +15,7 @@ const Home = React.memo(() => {
         <img
           src='https://scontent-iad3-1.xx.fbcdn.net/v/t1.0-9/60542009_10219123148534277_1429164283438039040_n.jpg?_nc_cat=106&_nc_oc=AQly8WbgVMyME79P2P7semUDgi0XemydnCl9ZDaOnubPf3NAFkNAbhDpRXtwr0UNG3g&_nc_ht=scontent-iad3-1.xx&oh=ad6c49a4c9f13c26142f0b4485b2c3ce&oe=5E11E76D'
           alt="Nisa"
+          width="500" height="600" 
           className="NisaPortrait"
         />
       </div>
--- folio/src/components/Projects.js
+++ folio/src/components/Projects.js
@@ -1,20 +1,26 @@
 import React from "react";
+import PokeDex from './pokedexfolio.png'
+import Hogwarts from './hogwarts.png'
+import Todo from './todofolio.png'
 
 export default function Projects(){
     return(
         <div>
             <div className='projects'>
                 <h3>Hogwarts Sorting Hat Quiz</h3>
+                <img src={Hogwarts} alt='hogwarts-preview' className='pokedex'/>
                 <a href='https://github.com/nisaChampagne/React-Sorting-Hat'>GitHub Link</a>
                 <a href='https://react-sorting-hat.nchampag23.now.sh/'>Hosted</a>
             </div>
             <div className='projects'>
             <h3>React Todo List</h3>
+            <img src={Todo} alt='todo-preview' className='pokedex'/>
                 <a href='https://github.com/nisaChampagne/React-Todo'>Github Link </a>
                 <a href='https://react-todo-4bm2326a5.now.sh/'>Hosted</a>
             </div>
             <div className='projects'>
                 <h3>Simple PokeDex</h3>
+                <img src={PokeDex} alt='pokedex-preview' className='pokedex'/>
                 <a href='https://github.com/nisaChampagne/Sideprojectpokedex'>Github Link</a>
                 <a href='https://pokedex.nchampag23.now.sh/'>Hosted</a>
             </div>

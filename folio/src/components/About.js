3d0a8d602b2773b7ff48be24373f8c423d481860
nisaChampagne <50988313+nisaChampagne@users.noreply.github.com>
2019-09-26 11:52:02 -0400

    updates

--- folio/src/components/About.js
+++ folio/src/components/About.js
@@ -51,7 +51,7 @@ class About2 extends React.Component{
     return(
         <div className="aboutContainer">
         <div className="Info">
-          <h1 className='aboutInfoh1'>My Stack</h1>
+          <h1 className='aboutInfoh1'>Skills</h1>
           <div className="text">
             <div className="aboutText">
               <h2 className="aboutInfoh2">Languages</h2>
@@ -62,11 +62,13 @@ class About2 extends React.Component{
               <br/>
               <p className='aboutInfoP'>JavaScript</p>
               <br/>
-              <p className='aboutInfoP'>React</p>
-              <br/>
               <p className='aboutInfoP'>Java</p>
-              <h2 className="aboutInfoh2">Frameworks</h2>
+              <h2 className="aboutInfoh2">Frameworks and Libraries</h2>
               <hr />
+              <p className='aboutInfoP'>ReactJS</p>
+              <br/>
+              <p className='aboutInfoP'>LESS/SASS</p>
+              <br/>
               <p className='aboutInfoP'>Bootstrap</p>
               <br/>
               <p className='aboutInfoP'>Semantic-UI</p>
@@ -74,6 +76,27 @@ class About2 extends React.Component{
               <p className='aboutInfoP'>Redux</p>
               <br/>
               <p className='aboutInfoP'>Styled-Components</p>
+              <h2 className="aboutInfoh2">Databases</h2>
+              <hr />
+              <p className='aboutInfoP'>PostgeSQL</p>
+              <h2 className="aboutInfoh2">Version Control</h2>
+              <hr />
+              <p className='aboutInfoP'>Git</p>
+              <br/>
+              <p className='aboutInfoP'>Github</p>
+              <br/>
+              <p className='aboutInfoP'>Heroku</p>
+              <br/>
+              <p className='aboutInfoP'>Zeit</p>
+              <br/>
+              <p className='aboutInfoP'>Terminal</p>
+              <br/>
+              <p className='aboutInfoP'>Windows</p>
+              <h2 className="aboutInfoh2">Software Development</h2>
+              <hr />
+              <p className='aboutInfoP'>Pair Programming</p>
+              <br/>
+              <p className='aboutInfoP'>Object Oriented Programming</p>
             </div>
           </div>
         </div>

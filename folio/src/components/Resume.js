335be3c9b7f3db938d9ecd490b57babf33c5b450
nisaChampagne <50988313+nisaChampagne@users.noreply.github.com>
2019-09-30 17:41:51 -0400

    adjusted resume tab

--- folio/src/components/Resume.js
+++ folio/src/components/Resume.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import PDF from './Nisa-Champagne2019.pdf'
+import PDF from './nisaChampagne2019resume.pdf'
 
 export default function Resume(){
 
     return(
         <div className='resumeHolder'>
-        <a href={PDF} download className='download'className="resumeBtn">Click to download resume</a>
+            <iframe className="resume"src="https://resume.creddle.io/embed/gj9aeancodo"
+             height="1100" seamless></iframe>
+            <a href={PDF} download className='download'className="resumeBtn">Click to download resume</a>
         </div>
     )
 }

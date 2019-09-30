228641664cf924a0cdd4c5bf28ef61f23a56535b
nisaChampagne <50988313+nisaChampagne@users.noreply.github.com>
2019-09-30 17:53:07 -0400

    adjusted resume tab

--- folio/src/components/Resume.js
+++ folio/src/components/Resume.js
@@ -5,8 +5,8 @@ export default function Resume(){
 
     return(
         <div className='resumeHolder'>
-            <iframe className="resume"src="https://resume.creddle.io/embed/gj9aeancodo"
-             height="1100" seamless></iframe>
+            {/* <iframe className="resume"src="https://resume.creddle.io/embed/gj9aeancodo"
+             height="1100" seamless></iframe> */}
             <a href={PDF} download className='download'className="resumeBtn">Click to download resume</a>
         </div>
     )
